a24642e6299778f80864b9bb9327fe68f3c2261b
Federexo24 <Fedeadamom2004@gmail.com>
2025-05-17 18:05:06 -0300

    Add files via upload

--- adamo-castro-cespedes-villagran-tp-p1/src/juego/Juego.java
+++ adamo-castro-cespedes-villagran-tp-p1/src/juego/Juego.java
@@ -1,5 +1,5 @@
 package juego;
-import java.awt.Color;
+
 import java.awt.Image;
 import entorno.Entorno;
 import entorno.Herramientas;
@@ -10,33 +10,28 @@ public class Juego extends InterfaceJuego
 	// El objeto Entorno que controla el tiempo y otros
 	private Entorno entorno;
 	private Image fondo;
-	// Variables y métodos propios de cada grupo
-	// ...
+	private Gondolf gondolf;
+
 	Juego()
 	{
 		// Inicializa el objeto entorno
 		this.entorno = new Entorno(this, "TP_GRUPO_13", 800, 600);
 		this.fondo = Herramientas.cargarImagen("imagenes/fondo.png");
-		// Inicializar lo que haga falta para el juego
-		// ...
-		// Inicia el juego!
+		this.gondolf = new Gondolf(400, 300);
+
+		// Inicia el juego
 		this.entorno.iniciar();
 	}
-	/**
-	 * Durante el juego, el método tick() será ejecutado en cada instante y 
-	 * por lo tanto es el método más importante de esta clase. Aquí se debe 
-	 * actualizar el estado interno del juego para simular el paso del tiempo 
-	 * (ver el enunciado del TP para mayor detalle).
-	 */
+
 	public void tick()
 	{
 	    // Dibujar imagen de fondo
 	    entorno.dibujarImagen(fondo, 400, 300, 0);
 
-	    
-	   // Aca va el resto del procesamiento del juego (mago, enemigos, etc.)
+	    // Mover y dibujar al mago
+	    gondolf.mover(entorno);
+	    gondolf.dibujar(entorno);
 	}
-	
 
 	@SuppressWarnings("unused")
 	public static void main(String[] args)
